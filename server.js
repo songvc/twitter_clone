ab01cb70325b5e7c5f5870d24aa35e9f9cb21e29
Vincent Song <songvc@users.noreply.github.com>
2016-02-17 01:43:34 -0800

    chirp api

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var login = require('./login');
 
 express()
   .set('view engine', 'ejs')
   .use(express.static('./public'))
-  .get('*', function(req, res) {
-    res.render('index.ejs');
+  .use(login.routes)
+  .use(require('./chirps'))
+  .get('*', login.required,  function(req, res) {
+    res.render('index', {
+      user: login.safe(req.user)
+    });
+
   })
   .listen(3000);
